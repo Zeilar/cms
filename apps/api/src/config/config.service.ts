c42be847d28ad1d45063a4d6fc6fbc7cbf594479
PhilipAngelinNE <philip.angelin@ne.se>
2022-09-24 23:51:04 +0200

    feat: typeorm module

--- apps/api/src/config/config.service.ts
+++ apps/api/src/config/config.service.ts
@@ -3,13 +3,30 @@ import { Injectable } from "@nestjs/common";
 interface Env {
 	PORT: number;
 	CORS_ORIGIN: string;
+	DB_TYPE: string;
+	DB_HOST: string;
+	DB_PORT: number;
+	DB_USERNAME: string;
+	DB_PASSWORD: string;
+	DB_NAME: string;
+	DB_SYNCHRONIZE: boolean;
+	DB_LOGGING: boolean;
 }
 
-const { PORT, CORS_ORIGIN } = process.env;
+const { PORT, CORS_ORIGIN, DB_TYPE, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_SYNCHRONIZE, DB_LOGGING } =
+	process.env;
 
 const ENV: Env = {
 	PORT: parseInt(PORT),
 	CORS_ORIGIN,
+	DB_TYPE,
+	DB_HOST,
+	DB_PORT: parseInt(DB_PORT),
+	DB_USERNAME,
+	DB_PASSWORD,
+	DB_NAME,
+	DB_SYNCHRONIZE: DB_SYNCHRONIZE === "true",
+	DB_LOGGING: DB_LOGGING === "true",
 };
 
 @Injectable()
--- apps/api/src/core/core.module.ts
+++ apps/api/src/core/core.module.ts
@@ -1,8 +1,37 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "../config/config.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigService } from "../config/config.service";
+
+/* 
+    DB_TYPE=postgres
+DB_HOST=db
+DB_PORT=5432
+DB_USERNAME=root
+DB_PASSWORD=eI0l._qW1z
+DB_NAME=whackakey
+DB_SYNCHRONIZE=false
+DB_LOGGING=false
+*/
 
 @Module({
-	imports: [ConfigModule],
-	exports: [ConfigModule],
+	imports: [
+		ConfigModule,
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			// @ts-expect-error type bug
+			useFactory: async (configService: ConfigService) => ({
+				type: configService.get("DB_TYPE"),
+				host: configService.get("DB_HOST"),
+				port: configService.get("DB_PORT"),
+				username: configService.get("DB_USERNAME"),
+				password: configService.get("DB_PASSWORD"),
+				database: configService.get("DB_NAME"),
+				synchronize: configService.get("DB_SYNCHRONIZE"),
+				logging: configService.get("DB_LOGGING"),
+			}),
+		}),
+	],
 })
 export class CoreModule {}
