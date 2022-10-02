c4bfc1d86156cbecb10f855b4b958536f8484582
Zeilar <philip_angelin@hotmail.com>
2022-10-03 01:04:27 +0200

    feat: add knex connection

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,16 @@
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import knex from "knex";
+import { Model } from "objection";
 import { ConfigService } from "./config/config.service";
 import { CoreModule } from "./core/core.module";
+import connection from "../knexfile";
 
 async function bootstrap() {
 	const app = await NestFactory.create(CoreModule);
 	const configService = app.get(ConfigService);
+
+	Model.knex(knex(connection));
 
 	app.useGlobalPipes(new ValidationPipe({ transform: true })).enableCors({
 		origin: configService.get("CORS_ORIGIN"),
